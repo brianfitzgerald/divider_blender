87ef9cb2a78687c0be3374a0188c02818a2e2ede
Brian Fitzgerald <brian.fitzgerald@equipmentshare.com>
2020-04-15 17:25:59 -0500

    good lord

--- __init__.py
+++ __init__.py
@@ -37,20 +37,26 @@ def main(context, num_subdivisions, offset, extrude):
     decoy.data = obj.data.copy()
     bpy.context.scene.collection.objects.link(decoy)
     basis = create_offset_bmesh(context, obj, num_subdivisions, offset, extrude)
+    basis.to_mesh(obj.data)
     if animate:
         # end frame
-        begin_frame = basis.verts.layers.shape.new('Begin')
-        end_frame = basis.verts.layers.shape.new('End')
+        begin_frame = obj.shape_key_add(name="begin")
+        end_frame = obj.shape_key_add(name="end")
+        begin_frame.interpolation = 'KEY_LINEAR'
+        end_frame.interpolation = 'KEY_LINEAR'
         animated = create_offset_bmesh(context, decoy, num_subdivisions, 0.5, extrude)
         animated.to_mesh(decoy.data)
         print(len(basis.verts), len(animated.verts))
         basis.verts.ensure_lookup_table()
         animated.verts.ensure_lookup_table()
-        for u in range(len(basis.verts)):
-            basis.verts[u][begin_frame] = basis.verts[u].co
-            basis.verts[u][end_frame] = animated.verts[u].co
-            print(basis.verts[u][begin_frame],basis.verts[u][end_frame])
         basis.to_mesh(obj.data)
+        for i in range(len(end_frame.data)):
+            print(end_frame.data[i].co, basis.verts[i].co)
+        for i in range(len(basis.verts)):
+            end_frame.data[i].co = animated.verts[i].co
+            begin_frame.data[i].co = basis.verts[i].co
+            print(end_frame.data[i].co, animated.verts[i].co, basis.verts[i].co)
+        animated.to_mesh(decoy.data)
 
 def create_offset_bmesh(context, obj, num_subdivisions, offset, extrude):
     bm = bmesh.new()
